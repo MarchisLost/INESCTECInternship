85957bcb804464ee20eda029e64daed9c3831077
David Pinto <davidpinto15@gmail.com>
2021-07-13 12:17:15 +0100

    added constraints

--- main.py
+++ main.py
@@ -14,6 +14,23 @@ def minimalUAVNumbers():
     eMBB_areas = [(45, 25, 0), (5, 45, 0), (55, 45, 0), (35, 75, 0), (45, 75, 0)]
     URLLC_areas = [(65, 15, 0), (75, 15, 0), (15, 45, 0), (75, 55, 0), (25, 75, 0)]
 
-    
+
+    # Add first contraint about "Number of RUs provided by UAVi"
+    ri = model.BoolVar('ri')
+    Ri = 20
+    model.Add(rim <= Ri * ri).OnlyEnforceIf(ri.Not())
+
+    # Add second contraint about "one subarea being served by only 1 UAV"
+    Pim = model.BoolVar(Pim)
+    model.Add(Pim == 1)
+
+    # Add third contraint about "Bidirectional network capacity provided by an RU of UAVi" eMBB
+    TeMBB = 13000000
+    model.Add(Cim * rim * Pim >= TeMBB)
+
+    # Add third contraint about "Bidirectional network capacity provided by an RU of UAVi" URLLC
+    TURLLC = 6500000
+    model.Add(Cim * rim * Pim >= TURLLC)
+
     
 # minimalUAVNumbers()
